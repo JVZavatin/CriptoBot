0927a14c37ed2c1af3d765919ea10461843cb428
herculests <henriquesantos@alunos.utfpr.edu.br>
2022-05-30 20:31:53 -0300

    Adição dos termos

---
+++ actions/crypto_terms.py
@@ -0,0 +1,106 @@
+terms_dict = {
+    "Addy" : "Endereço de uma carteira de criptomoeda.",
+    "Altcoin" : "Nome dado as moedas alternativas ao bitcoin. Exemplo: Litecoin, Dogecoin, Dash, etc.",
+    "ATH" : "O preço máximo que uma determinada criptomoeda já atingiu.",
+    "Ativos" : "Se refere a qualquer pertence que componha os bens de uma pessoa. Por exemplo, se você possui R$ 3.000,00 em sua conta bancária, esses são os seus ativos nesse banco. No caso das criptomoedas, chamamos de “Ativos Digitais”.",
+    "ATM" : "Automated Teller Machine, que significa: Caixa Eletrônico. No caso do Bitcoin, às vezes são chamados de BTM, permite que os usuários façam compra e venda de bitcoins, usando dinheiro físico ou cartões de débito.",
+    "Baleia" : "Detentor de grande parte de uma determinada moeda, a baleia é um usuário que centraliza a moeda controlando o preço dela.",
+    "Bear" : "Do inglês, “Urso”. O “Bear” é o investidor que crê na queda do preço da criptomoeda à qualquer momento. Com isso, o Bear vende seus ativos antes que desvalorizem demais. Quando dizemos que um mercado é “Bearish”, quer dizer que naquele momento há mais ordens de venda do que de compra.",
+    "Bearish" : "É um comportamento agressivo do gráfico de cima para baixo (caracterizado por uma descida grande e uma subida curta).",
+    "Bid" : "O Bid é o preço mais alto que um determinado comprador está disposto a pagar naquela transação. Ele é o valor que os compradores oferecem para o ativo.",
+    "Block Explorer" : "Também conhecido como Blockchain Browser, é um site ou programa de computador que permite você visualizar as transações, endereços, blocos e qualquer informação de uma blockchain e de uma criptomoeda específica.",
+    "Blockchain" : "A blockchain também conhecida por protocolo de confiança, é uma tecnologia de registro distribuído que visa a descentralização como medida de segurança. É vista como a principal inovação tecnológica do Bitcoin visto que é a prova de todas as transações na rede. Registra e guarda todas as transações feitas na história do Bitcoin.",
+    "Bloco Genesis" : "O primeiro bloco do Bitcoin, minerado por Satoshi Nakamoto.",
+    "Bull" : "Do inglês, significa “Touro” e é exatamente o contrário do “Bear”. O bull é o investidor que crê na evolução do preço da criptomoeda. Esse usuário aposta em comprar a moeda na baixa para fazer lucro quando o valor subir.",
+    "Bullish" : "É um comportamento agressivo do gráfico de baixo pra cima (caracterizado por uma subida grande e uma descida curta).",
+    "Candlestick" : "Candlestick é uma representação gráfica do preço de um ativo. Isso permite que seja possível visualizar os preços de abertura, alta, baixa e fechamento dentro de um período de tempo no gráfico.",
+    "Carteira" : "Em inglês “wallet” é onde o investidor pode guardar suas moedas digitais de forma mais segura até o momento de venda e/ou troca.",
+    "Cold storage" : "Movimentação de criptomoedas offline, ou seja, armazenar as criptos em carteiras de papel.",
+    "Criptografia" : "A criptografia é um conjunto de codificações feitas para proteger uma informação de modo que apenas o emissor e o receptor possam compreender.",
+    "Criptomoeda" : "O termo criptomoeda, é utilizado para referir-se a moedas digitais como por exemplo o Bitcoin, que usa constantemente a criptografia para vários fins. Um deles é garantir que todas as transações sejam feitas de forma 100% segura.",
+    "Custódia" : "O termo vem de custodiar, ou seja, possuir uma propriedades de ativos que tenha o seu controle. Ter sob custódia uma carteira ou ativos, também pode significar manter suas chaves privadas e em sigilo.",
+    "Cypherpunk" : "Cypherpunk é uma comunidade de defensores da privacidade e do anonimato online. Seu lema é Cypherpunks write code (Cypherpunks escrevem códigos) e acreditam que aqueles que desejam privacidade devem ir buscá-la por conta própria em vez de esperar que os outros façam para si.",
+    "Day Trader" : "Trader que faz movimentações diárias, comprando e vendendo.",
+    "DDoS" : "Distributed Denial of Service, em português: Ataque Distribuído de Negação de Serviços. Este ataque que utiliza um grande número de computadores sob o controle de um atacante para enviar pequenas quantidades de tráfegos pela internet com o objetivo de congestionar o acesso e drenar recursos de um servidor alvo.",
+    "Dump" : "Quando o preço de uma criptomoeda desce inesperadamente.",
+    "Dust transaction" : "Transação com uma pequena quantidade de bitcoins, com baixo valor financeiro, mas que ocupa espaço no blockchain.",
+    "Escrow" : "Nome dado pelo ato de manter fundos em posse de terceiros, a fim de se proteger durante uma operação.",
+    "Ethereum" : "Ethereum é uma plataforma que permite a programação de aplicativos descentralizados, contratos inteligentes e transações da criptomoeda Ether e vários tokens.",
+    "Exchange" : "Local utilizado para troca entre criptomoedas e outros ativos, por exemplo, trocar real por bitcoin. Exchange de bitcoins são utilizadas para trocar bitcoin por moedas FIAT ou outras criptomoedas.",
+    "Faucet" : "Sites que oferecem recompensas em bitcoin a partir de cliques em propagandas ou realizar pequenas tarefas. Exemplo: responder pesquisas.",
+    "Fiat" : "É o dinheiro fiduciário, ou seja, aquele que não é criptomoeda, como o Real, Dólar, Euro, Iene, etc.",
+    "FOMO" : "Fear of missing out, em português: medo de perder uma oportunidade que pode gerar lucro.",
+    "Fork" : "Atualizações nos códigos de criptografia das moedas geram uma bifurcação, chamada de Fork. Uma nova moeda gerada a partir de outra, como o Bitcoin Gold que é um fork do Bitcoin.",
+    "Halving" : "O halving do Bitcoin, é uma é uma característica que está encravada dentro do código da criptomoeda. Diferente dos sistemas monetários atuais nos quais os governos imprimem dinheiro sem parar, o bitcoin reduz sua emissão a cada 4 anos.",
+    "Hash" : "É um algoritmo utilizado pelo protocolo do bitcoin e de outras criptomoedas para transformar um grande número de informações em uma sequência numérica hexadecimal de tamanho fixo.",
+    "Hash rate" : "Número de hashes processados por um minerador em um determinado período de tempo.",
+    "HODL" : "É um meme, o correto seria Hold – de “segurar”, do inglês, que é quando você mantém seus ativos, mesmo na baixa de preço, pois acredita que será valorizado futuramente.",
+    "Hot Wallet" : "É uma carteira de criptomoedas que está online e conectada com a Internet.",
+    "ICO" : "Initial Coin Offering, em português, Oferta Inicial de Moeda. É um sistema criado para arrecadar fundos para uma start-up ou empresa. Normalmente elas surgem com “ideias revolucionárias ou únicas” que são aplicadas em cima de uma blockchain. Atenção: ICOs também podem ser Scams.",
+    "Input" : "Endereço de origem de uma transação bitcoin. Uma única transação pode ter múltiplos endereços de origem.",
+    "Lastro" : "É um ativo que tem como objetivo dar uma garantia, ou seja, ele relaciona um ativo a princípio sem valor, com um algo que possua um valor implícito.",
+    "Ledger" : "O ledger é um registro compartilhado de informações, a exemplo um livro caixa de um banco, um livro onde ficam registrados todas as transações financeiras feitas em um banco, o ledger seria um local onde ficam registradas as transações feitas por criptomoedas, que passam pela Blockchain.",
+    "Liquidez" : "É a capacidade de comprar ou vender um ativo facilmente, mesmo em grandes quantidades.",
+    "Maleabilidade" : "Habilidade de modificar transações não confirmadas sem fazê-las inválidas.",
+    "Maker" : "Maker é um termo usado quando inclui uma ordem e ela não é negociada imediatamente. Onde ela permanece no livro de ofertas e aguarda que outra pessoa envie uma ordem contrária para que ela seja executada.",
+    "Marketcap" : "Em português: capitalização de mercado. Quantidade de criptomoeda circulante X preço da cripto.",
+    "Mineração" : "É o ato de realizar cálculos matemáticos. Quando um computador realiza esse cálculo criptográfico ele recebe uma recompensa, X Bitcoin. Dizemos que ele está minerando e permitindo que surja mais Bitcoin.",
+    "Mixer" : "Serviço utilizado para embaralhar input e output de transações, a fim de manter a privacidade e diminuir o nível de rastreamento.",
+    "Output" : "Endereço destino de uma transação bitcoin. É possível que uma transação tenha múltiplos outputs.",
+    "P2P" : "P2P significa peer-to-peer, em português: ponto-a-ponto. O Bitcoin foi projetado como um sistema peer-to-peer, ou seja, que não precisa de intermediários, como bancos centrais, para intermediar uma transação entre duas pessoas. No mercado, este termo é usado para descrever comercialização de criptomoedas entre duas partes, sem a necessidade de uma exchange.",
+    "Paper Wallet" : "É considerado um meio muito seguro de guardar suas criptomoedas, pois não correm tanto risco, justamente por ser offline. Pois não é necessário um acesso a internet, ela é basicamente um pedaço de papel contendo suas chaves privadas e públicas.",
+    "Pool" : "Coleção de mineradores que se agrupam para minerar coletivamente um bloco, e depois dividir a recompensa entre eles. Pools de mineração são uma ótima maneira para aumentar a probabilidade de êxito conforme a dificuldade for aumentando.",
+    "PoW" : "É a prova de que uma transação foi validada e é legítima. Por meio de uma função matemática (a SHA-256), as transações são codificadas e enviadas para a rede, onde os mineradores competem entre si para decodificá-las.",
+    "Profit" : "Lucros obtidos.",
+    "Pump" : "Quando o preço de uma moeda sobe inesperadamente.",
+    "ROI" : "O retorno que se tem baseado no quanto você investiu.",
+    "Satoshi" : "Menor divisão de um Bitcoin = 0,00000001 BTC. Quando alguém fala que tem 10 Satoshis, significa que possui 0,00000010 BTC.",
+    "Satoshi Nakamoto" : "Pseudônimo usado para o criador do Bitcoin.",
+    "Scamcoin" : "Altcoin criada com objetivos de dar golpe nos usuários e enriquecer os criadores.",
+    "SHA-256" : "Função matemática do tipo hash utilizando no bitcoin em diversos contextos, inclusive durante o processo de mineração.",
+    "ShitCoin" : "É o termo utilizado para classificar moedas scams, com baixa ou nenhuma reputação na comunidade.",
+    "Spread" : "Diferença entre o preço de compra e o preço de venda no livro de ofertas (book).",
+    "Swingtrader" : "Estratégia de trade com poucas operações ao longo do tempo. Se aproveita das ondas (swings) do mercado.",
+    "Taker" : "Taker é o investidor que possui uma ordem que é instantaneamente executada pois encontra outra ordem contrária.",
+    "Testnet" : "Uma rede alternativa ao Bitcoin usada para testes.",
+    "Ticker" : "É o nome dado aos símbolos das moedas: BTC (Bitcoin), ETH (Ethereum), LTC (Litecoin), XRP (Ripple), ADA (Cardano).",
+    "Token" : "Normalmente os tokens são confundidos com criptomoedas, porém existem diferenças. As criptomoedas são moedas criadas com o propósito de serem moedas, já os tokens são criados para serem distribuídos a pessoas com promessas de valerem algo no futuro. Comparando ambos ao mercado convencional de investimentos, as criptomoedas poderiam ser moedas como USD ou EURO, já os Tokens seriam as Ações de uma empresa.",
+    "Trade" : "Operação de compra e venda de alguma ciptomoeda. Quando você deposita 100 reais, compra X Bitcoin e depois vende, você está fazendo uma operação de trading.",
+    "TXID" : "Mais conhecido como hash da transação. Este é um identificador usado para referenciar transações em uma blockchain.",
+    "Volatilidade" : "Movimentos dos preços de um ativo. Se o valor do ativo sobe e desce com muita frequência, às vezes até de diferenças de preço grandes, diz-se que o ativo tem alta volatilidade.",
+    "Wallet" : "Em português “carteira” é onde o investidor pode guardar suas moedas digitais de forma mais segura até o momento de venda e/ou troca.",
+    "Withdrawal" : "Retirada de algum valor, como um saque.",
+    "XBT" : "Representa a unidade monetária do bitcoin.",
+    "CBDC" : "Dinheiro fiduciário, porém no formato digital, emitido por governos e Bancos Centrais. Não há necessariamente o uso de um blockchain, pois o emissor e controlador possui total liberdade para mudar regras de emissão, congelar e reverter transações, e até mesmo banir determinados endereços.",
+    "Coinjoin" : "Protocolo descentralizado que mistura as moedas de diferentes usuários, permitindo melhorar a privacidade. Também conhecido como mixer, tenta ofuscar a origem da moeda e identidade de seu dono através das ferramentas de análises de histórico do blockchain.",
+    "Criptoativo" : "Ao contrário das criptomoedas, não possuem blockchain próprio. São conhecidos como ativos digitais, porém protegidos pela criptografia e segurança de alguma criptomoeda existente. Pode ser utilizada para a representação digital de um ativo real, por exemplo, um Título Precatório, ou até mesmo um imóvel.",
+    "Fees" : "Taxa cobrada quando uma transação é registrada em uma blockchain pelos mineradores. Também pode ser utilizado no caso das taxas de corretagem.",
+    "Mempool" : "Abreviação de “Memory Pool”, o conjunto de transações que ainda não foram confirmadas em um blockchain. Toda transação é direcionada para o mempool, e aguarda que os mineradores as agreguem na construção dos blocos. Transações com taxa mais alta usualmente são confirmadas primeiro.",
+    "NFT" : "Acrônimo de “Not Fungible Tokens”, ou “tokens não fungíveis”. Solução criada para permitir representar objetos com qualidades únicas em um blockchain, por exemplo, o registro de um imóvel ou obra de arte digital.",
+    "Taproot" : "Atualização do Bitcoin que será implementada em novembro. Possibilita que o Bitcoin trabalhe com alguns scripts (códigos programáveis), e tenha mais privacidade nas transações mais complexas.",
+    "Stablecoin" : "Criptomoeda criada para ter valor estável, usualmente em dólar. Existem modelos que trabalham com um lastro real, ou seja, valores depositados em banco e equivalente, enquanto outros atuam através de balanceamento através de smart contracts (vide termo).",
+    "Trezor" : "Uma das principais fabricantes de cold wallets, ao lado da Coldcard e Ledger.",
+    "Whitepaper" : "Documento técnico que descreve as propriedades de um projeto, comum na indústria de criptomoedas."
+}
+
+# Checa se existe o termo na lista
+# Parâmetros: term - string
+# Retorno: boolean
+def check_term(term):
+    return term in terms_dict
+
+# Obtem o nome do termo
+# Parâmetros: term - string
+# Retorno: string
+def get_term(term):
+    return terms_dict.get(term, 'Termo não encontrado')
+
+# Remove um termo
+# Parâmetros: term - string
+def del_term(term):
+    del terms_dict[term]
+
+# Adiciona um termo
+# Parâmetros: term - string, description -string
+def add_term(term, description):
+    terms_dict[term] = description
